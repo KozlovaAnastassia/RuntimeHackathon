097ab568bc3eba9190c5b7e60d94063cd56919f4
ruagouy <anastasia.a.kozlova@gmail.com>
2025-08-22 13:36:25 +0300

    Add project

---
+++ RuntimeHackathon/RuntimeHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuntimeHackathonApp.swift
+//  RuntimeHackathon
+//
+//  Created by KOZLOVA Anastasia on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct RuntimeHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
